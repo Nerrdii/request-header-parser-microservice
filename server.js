e3806ed95b9a990002312f049fb372f1a07f9b59
Joseph Davis <soccermaniac898@gmail.com>
2017-03-19 16:36:25 -0500

    Add index page

--- server.js
+++ server.js
@@ -6,9 +6,17 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-    res.send("It works!");
+    var file = path.join(__dirname, "index.html");
 
-    res.end();
+    res.sendFile(file, function(err){
+        if (err) {
+            res.statusCode = err.status;
+            console.log("Couldn't serve file: " + file);
+            console.log("Error: " + err.message);
+        } else {
+            console.log("Serving file: " + file);
+        }
+    });
 });
 
 app.listen(port, function(){
