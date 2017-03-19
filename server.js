bfad8f7d7ee26f4117d05de91123ace6c2d5af16
Joseph Davis <soccermaniac898@gmail.com>
2017-03-19 16:46:19 -0500

    Add API endpoint

--- server.js
+++ server.js
@@ -19,6 +19,22 @@ app.get('/', function(req, res){
     });
 });
 
+app.get('/api', function(req, res){
+    var ip = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+    var language = req.headers["accept-language"];
+    var software = req.headers['user-agent'];
+    software = software.substring(software.indexOf("(")+1, software.indexOf(")"));
+
+    res.json({
+        "ip-address": ip,
+        "language": language,
+        "software": software
+    });
+});
+
 app.listen(port, function(){
     console.log("App listening on port " + port);
 });
