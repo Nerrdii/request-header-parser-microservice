16395590399b90368bababae6fa90f714dbf17dc
Joseph Davis <soccermaniac898@gmail.com>
2017-11-11 10:46:41 -0600

    Update server file

--- server.js
+++ server.js
@@ -1,31 +1,19 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
 
-var app = express();
+const app = express();
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-    var file = path.join(__dirname, "index.html");
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
-    res.sendFile(file, function(err){
-        if (err) {
-            res.statusCode = err.status;
-            console.log("Couldn't serve file: " + file);
-            console.log("Error: " + err.message);
-        } else {
-            console.log("Serving file: " + file);
-        }
-    });
-});
-
-app.get('/api', function(req, res){
-    var ip = req.headers['x-forwarded-for'] ||
+app.get('/api', (req, res) => {
+    const ip = req.headers['x-forwarded-for'] ||
         req.connection.remoteAddress ||
         req.socket.remoteAddress ||
         req.connection.socket.remoteAddress;
-    var language = req.headers["accept-language"].split(",")[0];
-    var software = req.headers['user-agent'];
+    const language = req.headers["accept-language"].split(",")[0];
+    let software = req.headers['user-agent'];
     software = software.substring(software.indexOf("(")+1, software.indexOf(")"));
 
     res.json({
@@ -35,6 +23,4 @@ app.get('/api', function(req, res){
     });
 });
 
-app.listen(port, function(){
-    console.log("App listening on port " + port);
-});
+app.listen(port, () => console.log(`App now listening on port ${port}...`));
